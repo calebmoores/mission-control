a32ca94813ac1bdbf92773783568828f550ee4dc
Caleb <caleb@example.com>
2026-02-14 01:02:18 -0800

    Add static HTML dashboard and update server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/app/page.tsx'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Serve the test page
